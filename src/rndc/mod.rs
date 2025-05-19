5033b4d7b09cb86a90392e1892dacb7d7a86a75a
kweonminsung <kevin136583@gmail.com>
2025-05-19 11:24:06 +0900

    feat: added rndc client

--- src/rndc/mod.rs
+++ src/rndc/mod.rs
@@ -1,1 +1,11 @@
+use lazy_static::lazy_static;
 use rndc::RndcClient;
+
+lazy_static! {
+    pub static ref RNDC_CLIENT: RndcClient = {
+        let server_url = crate::env::get_env("RNDC_SERVER_URL");
+        let algorithm = crate::env::get_env("RNDC_ALGORITHM");
+        let secret_key = crate::env::get_env("RNDC_SECRET_KEY");
+        RndcClient::new(&server_url, &algorithm, &secret_key)
+    };
+}
